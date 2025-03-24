54bd215f8fcc22e9f43ce62e4d67f546dbd72154
Kamdin Moti-Shari <kamdinmoti-shari@Mac.fritz.box>
2025-03-24 15:23:57 +0100

    t

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,6 @@ import App from "./App.tsx";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./style/index.css";
 
-if (process.env.NODE_ENV === 'development') {
-  import('react-devtools').then(() => {
-    console.log('React DevTools enabled');
-  });
-}
-
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
