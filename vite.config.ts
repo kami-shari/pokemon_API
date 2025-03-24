0d0f5147502de68f6f296efc66bf8761c977b875
Kamdin Moti-Shari <kamdinmoti-shari@Mac.fritz.box>
2025-03-24 14:24:49 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 80,
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'https://pokeapi.co',
