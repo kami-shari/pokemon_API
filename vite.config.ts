2b237f3454263245ba926218648670abe6bd3029
Kamdin Moti-Shari <kamdinmoti-shari@Mac.fritz.box>
2025-03-24 14:54:10 +0100

    t

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ export default defineConfig({
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 10000,
     host: '0.0.0.0',
-    allowedHosts: ['pokemon-api-79sn.onrender.com'],
+    allowedHosts: ['pokemon-api-79sn.onrender.com', 'localhost'],
     proxy: {
       '/api': {
         target: 'https://pokeapi.co',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false
       }
     }
   },
