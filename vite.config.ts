ebb380da2e0c62cba20ac067d2cac9fafd4211e4
Kamdin Moti-Shari <kamdinmoti-shari@Mac.fritz.box>
2025-03-24 14:38:34 +0100

    t

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   server: {
-    port: process.env.PORT ? Number(process.env.PORT) : 80,
+    port: process.env.PORT ? Number(process.env.PORT) : 10000,
     host: '0.0.0.0',
     proxy: {
       '/api': {
